d6ff702e26f59197ef6ff8433c2e59a74b36dfc1
Wanlin Du <wanlindu@google.com>
2025-05-06 13:17:22 -0700

    feat: export the createJsonSchemaValidator methods to be available for other library.
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/googleapis/js-genai/pull/519 from googleapis:release-please--branches--main--components--genai 385304b52b680021c2827f605a1a2a0d1ff636d8
    PiperOrigin-RevId: 755495497

--- test/system/node/client_test.ts
+++ test/system/node/client_test.ts
@@ -7,12 +7,9 @@
 import {fail} from 'assert';
 import {GoogleAuthOptions} from 'google-auth-library';
 import {z} from 'zod';
+import {zodToJsonSchema} from 'zod-to-json-schema';
 
 import {GoogleGenAI} from '../../../src/node/node_client';
-import {
-  functionDeclarationFromZodFunction,
-  schemaFromZodType,
-} from '../../../src/schema_helper';
 import {
   FunctionCallingConfigMode,
   GenerateContentResponse,
@@ -161,7 +158,7 @@ describe('generateContent', () => {
       }
     }
   });
-  it('ML Dev should generate content with given zod schema', async () => {
+  it('ML Dev should generate content with given valid json schema', async () => {
     const innerObject = z.object({
       innerString: z.string(),
       innerNumber: z.number(),
@@ -193,7 +190,10 @@ describe('generateContent', () => {
       contents: 'populate the following object',
       config: {
         responseMimeType: 'application/json',
-        responseSchema: schemaFromZodType(nestedSchema),
+        responseSchema: zodToJsonSchema(nestedSchema) as Record<
+          string,
+          unknown
+        >,
       },
     });
     const parsedResponse = JSON.parse(
@@ -204,7 +204,7 @@ describe('generateContent', () => {
     expect(validationResult.success).toEqual(true);
   });
 
-  it('Vertex AI should generate content with given zod schema', async () => {
+  it('Vertex AI should generate content with given valid json schema', async () => {
     const innerObject = z.object({
       innerString: z.string(),
       innerNumber: z.number(),
@@ -241,7 +241,10 @@ describe('generateContent', () => {
       contents: 'populate the following object',
       config: {
         responseMimeType: 'application/json',
-        responseSchema: schemaFromZodType(nestedSchema),
+        responseSchema: zodToJsonSchema(nestedSchema) as Record<
+          string,
+          unknown
+        >,
       },
     });
     const parsedResponse = JSON.parse(
@@ -251,16 +254,12 @@ describe('generateContent', () => {
     const validationResult = nestedSchema.safeParse(parsedResponse);
     expect(validationResult.success).toEqual(true);
   });
-  it('ML Dev should generate function call with given zod function schema', async () => {
+  it('ML Dev should be able to help build the FunctionDeclaration', async () => {
     const stringArgument = z.object({
       firstString: z.string(),
       secondString: z.string(),
     });
-    const concatStringFunction = z
-      .function()
-      .args(stringArgument)
-      .returns(z.void())
-      .describe('this is a concat string function');
+
     const client = new GoogleGenAI({vertexai: false, apiKey: GOOGLE_API_KEY});
     const response = await client.models.generateContent({
       model: 'gemini-1.5-flash',
@@ -269,10 +268,14 @@ describe('generateContent', () => {
         tools: [
           {
             functionDeclarations: [
-              functionDeclarationFromZodFunction({
+              {
                 name: 'concatStringFunction',
-                zodFunctionSchema: concatStringFunction,
-              }),
+                description: 'this is a concat string function',
+                parameters: zodToJsonSchema(stringArgument) as Record<
+                  string,
+                  unknown
+                >,
+              },
             ],
           },
         ],
@@ -297,58 +300,11 @@ describe('generateContent', () => {
     });
   });
 });
-it('ML Dev should generate function call with given zod function schema no separate function parameter def', async () => {
-  const concatStringFunction = z
-    .function()
-    .args(z.object({firstString: z.string(), secondString: z.string()}))
-    .returns(z.void())
-    .describe('this is a concat string function');
-  const client = new GoogleGenAI({vertexai: false, apiKey: GOOGLE_API_KEY});
-  const response = await client.models.generateContent({
-    model: 'gemini-1.5-flash',
-    contents: 'put word: hello and word: world into a string',
-    config: {
-      tools: [
-        {
-          functionDeclarations: [
-            functionDeclarationFromZodFunction({
-              name: 'concatStringFunction',
-              zodFunctionSchema: concatStringFunction,
-            }),
-          ],
-        },
-      ],
-      toolConfig: {
-        functionCallingConfig: {
-          mode: FunctionCallingConfigMode.ANY,
-          allowedFunctionNames: ['concatStringFunction'],
-        },
-      },
-    },
-  });
-  const functionCallResponse =
-    response.candidates![0].content!['parts']![0].functionCall;
-  expect(functionCallResponse!.name).toEqual('concatStringFunction');
-  const parsedArgument =
-    concatStringFunction._def.args._def.items![0].safeParse(
-      functionCallResponse!.args!,
-    );
-  expect(parsedArgument.success).toEqual(true);
-  expect(parsedArgument.data).toEqual({
-    firstString: 'hello',
-    secondString: 'world',
-  });
-});
-it('Vertex AI should generate function call with given zod function schema', async () => {
+it('Vertex AI should be able to help build the FunctionDeclaration', async () => {
   const stringArgument = z.object({
     firstString: z.string(),
     secondString: z.string(),
   });
-  const concatStringFunction = z
-    .function()
-    .args(stringArgument)
-    .returns(z.string())
-    .describe('this is a concat string function');
   const client = new GoogleGenAI({
     vertexai: true,
     project: GOOGLE_CLOUD_PROJECT,
@@ -361,10 +317,14 @@ it('Vertex AI should generate function call with given zod function schema', asy
       tools: [
         {
           functionDeclarations: [
-            functionDeclarationFromZodFunction({
+            {
               name: 'concatStringFunction',
-              zodFunctionSchema: concatStringFunction,
-            }),
+              description: 'this is a concat string function',
+              parameters: zodToJsonSchema(stringArgument) as Record<
+                string,
+                unknown
+              >,
+            },
           ],
         },
       ],
