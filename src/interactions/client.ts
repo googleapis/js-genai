f31dd2a213afbdfbc5de3f0cb21f2c93fb903920
Wanlin Du <wanlindu@google.com>
2026-02-06 10:56:08 -0800

    chore: pull in generator change
    PiperOrigin-RevId: 866529470

--- src/interactions/client.ts
+++ src/interactions/client.ts
@@ -636,7 +636,6 @@ export class BaseGeminiNextGenAPIClient {
       return await this.fetch.call(undefined, url, fetchOptions);
     } finally {
       clearTimeout(timeout);
-      if (signal) signal.removeEventListener('abort', abort);
     }
   }
 
