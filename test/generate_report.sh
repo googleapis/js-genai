35b08d7fc38e1f448daa9ce6c01812133a1a49ed
Varun <vaarunn@google.com>
2026-09-02 13:25:29 +0530

    fix(test): correctly scope environment variables in coverage script

--- test/generate_report.sh
+++ test/generate_report.sh
@@ -19,7 +19,7 @@ fi
 function cleanup {
   echo "Cleaning up temp working directory $WORK_DIR" and default output directory for nyc $DEFAULT_NYC_OUTPUT_DIR
   rm -rf "$WORK_DIR"
-  rm -Rf DEFAULT_NYC_OUTPUT_DIR
+  rm -Rf "$DEFAULT_NYC_OUTPUT_DIR"
   echo "Deleted temp working directory $WORK_DIR"
 }
 
@@ -34,9 +34,8 @@ SYSTEM=coverage-system-test
 # Generate the reports for each test suite separately to avoid covering each
 # other.
 tsc
-export GOOGLE_API_KEY=googapikey GOOGLE_CLOUD_PROJECT=googcloudproj GOOGLE_CLOUD_LOCATION=googcloudloc
-c8  --exclude="src/private/**" --exclude="dist/src/private/**" --reporter=json --report-dir=./${WORK_DIR}/${UNIT} jasmine dist/test/unit/**/*_test.js dist/test/unit/*_test.js
-c8  --exclude="src/private/**" --exclude="dist/src/private/**" --reporter=json --report-dir=./${WORK_DIR}/${SYSTEM} jasmine dist/test/system/node/*_test.js -- --test-server
+GOOGLE_API_KEY=googapikey c8  --exclude="src/private/**" --exclude="dist/src/private/**" --reporter=json --report-dir=./${WORK_DIR}/${UNIT} jasmine dist/test/unit/**/*_test.js dist/test/unit/*_test.js
+GOOGLE_CLOUD_PROJECT=googcloudproj GOOGLE_CLOUD_LOCATION=googcloudloc c8  --exclude="src/private/**" --exclude="dist/src/private/**" --reporter=json --report-dir=./${WORK_DIR}/${SYSTEM} jasmine dist/test/system/node/*_test.js -- --test-server
 
 # Move all the generated coverage reports to the same directory to merge reports.
 mv ./${WORK_DIR}/${UNIT}/coverage-final.json  ./${WORK_DIR}/${UNIT}-coverage-report.json
@@ -44,7 +43,7 @@ mv ./${WORK_DIR}/${SYSTEM}/coverage-final.json  ./${WORK_DIR}/${SYSTEM}-coverage
 
 # Clean up the directory to avoid contamination, nyc will generate this
 # directory everytime.
-rm -Rf DEFAULT_NYC_OUTPUT_DIR || true
+rm -Rf "$DEFAULT_NYC_OUTPUT_DIR" || true
 
 # Merge the reports into one file.
 nyc merge ./${WORK_DIR} --output-file=${DEFAULT_NYC_OUTPUT_DIR}/coverage-report.json
